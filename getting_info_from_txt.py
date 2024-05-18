00fe9345f85cce5fd95a1d8d427d6103c9a41b5e
malah <malahovskijm14@bk.ru>
2024-05-18 10:34:55 +0300

    Created method, which getting url ssh path with commit hash in dictionary structure

---
+++ getting_info_from_txt.py
@@ -0,0 +1,23 @@
+import os
+
+path = r"sources.txt"
+
+
+# add searching by build number
+def getting_date_deep(path: str, build: str = None) -> dict:
+    url_hash = dict()
+    val = ''
+    key = ''
+    with open(path) as docs:
+        for line in docs:
+            if 'VCS URL' in line:
+                key = line.rstrip().split()[2]
+            if 'Commit hash' in line:
+                val = line.rstrip().split(':')[1]
+            if key != "" and val != "":
+                url_hash[key] = val
+
+    return url_hash
+
+
+print(getting_date_deep(path))
